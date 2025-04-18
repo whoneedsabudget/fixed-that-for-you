a589173d85a50ae6841f23f6138abef6a89829a4
Ryan Gibbons <rtgibbons23@gmail.com>
2025-04-18 15:46:32 -0500

    apple news custom parsing (#27)
    * upgrade dependencies
    
    * upgrade dependencies to minimums for python 3.13
    
    * Adding in ability for custom parsing of sources
    
    Initial parsing included for apple.news using beautifulsoup4.

---
+++ src/AppleNews.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+
+def replace_custom(url):
+  '''Apple News embedded link extractor'''
+  html_request = Request(url, headers={'User-Agent': 'Mozilla'})
+  html_response = urlopen(html_request).read()
+
+  html_soup = BeautifulSoup(html_response, 'html.parser')
+
+  embedded_article_url = html_soup.find_all('span', class_="click-here")[0].parent.get('href')
+
+  return f'https://archive.today/newest/{embedded_article_url}'
+  
+

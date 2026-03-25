f3149baabc43ea04626f379fd19df33a33f77e3c
andrew <r.andrew.fox@gmail.com>
2026-03-25 13:34:24 -0400

    Add Apple Stocks embedded link extractor function (#35)
    * Add Apple Stocks embedded link extractor function
    
    follows Custom parser methodology in title name
    
    * Rename StocksAppleCom to StocksAppleCom.py
    
    * Add stocks.apple.com to link parser
    
    adding apple stocks linking to use the custom parser

---
+++ src/StocksAppleCom.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+
+def replace_custom(url):
+  '''Apple Stocks embedded link extractor'''
+  html_request = Request(url, headers={'User-Agent': 'Mozilla'})
+  html_response = urlopen(html_request).read()
+
+  html_soup = BeautifulSoup(html_response, 'html.parser')
+
+  embedded_article_url = html_soup.find_all('span', class_="click-here")[0].parent.get('href')
+
+  return f'{embedded_article_url}'
+  
